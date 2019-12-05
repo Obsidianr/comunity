f631de66726714fd37aed294988b49d1e74c52d0
Obsidianr <18342205009@163.com>
2019-12-05 20:56:13 +0800

    add

---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,27 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("38dba2429e274411da64");
+        accessTokenDTO.setClient_secret("6f563938749684b97056cc17ce7c682545d6ad94");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        githubProvider.getAccessToken(accessTokenDTO);
+        return "index";
+    }
+}
